b0d0ca77f1407539a11ce9682762a3693e9802df
Fikri Kaymak <fikrikaymak@icloud.com>
2023-04-03 02:08:45 +0300

    first commit

---
+++ FlattenPro.py
@@ -0,0 +1,16 @@
+'''
+1- Write a function that flattens a list.
+Its elements can consist of multi-layered lists (such as [[3],2]) or non-scalar data. For example:
+
+input: [[1,'a',['cat'],2],[[[3]],'dog'],4,5]
+
+output: [1,'a','cat',2,3,'dog',4,5]
+'''
+'''
+def flatten():
+    l = []
+    l2 = input()
+    l.extend(l2)
+    l.sort
+    '''
+
